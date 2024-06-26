b3a3b317b807ad9bd4034b2ffbae46ff133445f5
Israel Sol <isrucrasus10@gmail.com>
2024-06-26 15:41:42 +0300

    added logout functionality

---
+++ recipe_app_backend/logout.php
@@ -0,0 +1,12 @@
+<?php
+ header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+header('Access-Control-Allow-Headers: token, Content-Type');
+header('Access-Control-Max-Age: 1728000');
+header('Content-Type: application/json'); // Correcting Content-Type to JSON
+
+echo json_encode('success');
+
+
+
+?>
