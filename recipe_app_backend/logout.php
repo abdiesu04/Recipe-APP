602e4c63402c4861f9680442cca03619bc09d069
Israel Sol <isrucrasus10@gmail.com>
2024-06-28 21:58:32 +0300

    inserted logout php file

--- recipe_app_backend/logout.php
+++ recipe_app_backend/logout.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Headers: token, Content-Type');
 header('Access-Control-Max-Age: 1728000');
 header('Content-Type: application/json'); // Correcting Content-Type to JSON
 
-echo json_encode('success');
+echo json_encode('success');//sends success for react frontend to redirect to sign in page
 
 
 
